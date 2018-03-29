64466deacc876842fc250bbbe3a0c94d8559497a
Babenko <babenkosemen@gmail.com>
2018-03-29 23:59:27 +0300

    Task 804 init

---
+++ src/main/java/com/sbabenko/lc/t804/UniqueMorseCodeWords.java
@@ -0,0 +1,17 @@
+package com.sbabenko.lc.t804;
+
+/**
+ * Created by Sem Babenko.
+ */
+public class UniqueMorseCodeWords {
+
+  private String[] morseAlphabet =
+      {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--",
+          "-.", "---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+
+  public int uniqueMorseRepresentations(String[] words) {
+
+    return 0;
+  }
+}
