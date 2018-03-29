583c5d5fbdf0006a36599b207447db870357d057
Babenko <babenkosemen@gmail.com>
2018-03-30 00:08:23 +0300

    Core logic for 804 task.

--- src/main/java/com/sbabenko/lc/t804/UniqueMorseCodeWords.java
+++ src/main/java/com/sbabenko/lc/t804/UniqueMorseCodeWords.java
@@ -1,9 +1,14 @@
 package com.sbabenko.lc.t804;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Sem Babenko.
  */
 public class UniqueMorseCodeWords {
+
+  private static final int CHARACTER_OFFSET = 96;
 
   private String[] morseAlphabet =
       {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--",
@@ -11,7 +16,15 @@ public class UniqueMorseCodeWords {
 
 
   public int uniqueMorseRepresentations(String[] words) {
-
-    return 0;
+    Set<String> result = new HashSet<>();
+    for (String word : words) {
+      StringBuilder newMorseWord = new StringBuilder();
+      for (int j = 0; j < word.length(); j++) {
+        int morseIndex = word.charAt(j) - CHARACTER_OFFSET;
+        newMorseWord.append(morseAlphabet[morseIndex]);
+      }
+      result.add(newMorseWord.toString());
+    }
+    return result.size();
   }
 }
