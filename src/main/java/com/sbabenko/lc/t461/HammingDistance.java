e4c150cae4480dcc3b539e712026564caadb4892
Babenko <babenkosemen@gmail.com>
2018-04-06 00:07:31 +0300

    Finish with 461

--- src/main/java/com/sbabenko/lc/t461/HammingDistance.java
+++ src/main/java/com/sbabenko/lc/t461/HammingDistance.java
@@ -6,6 +6,13 @@ package com.sbabenko.lc.t461;
 public class HammingDistance {
 
   public int hammingDistance(int x, int y) {
-    return x ^ y;
+    int res = 0;
+    int cursor = 1;
+    for (int i = 0; i < 32; i++, cursor <<= 1) {
+      if ((x & cursor) != (y & cursor)) {
+        res++;
+      }
+    }
+    return res;
   }
 }
--- src/test/java/com/sbabenko/lc/t461/HammingDistanceTest.java
+++ src/test/java/com/sbabenko/lc/t461/HammingDistanceTest.java
@@ -2,6 +2,7 @@ package com.sbabenko.lc.t461;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -11,5 +12,9 @@ class HammingDistanceTest {
 
   @Test
   void hammingDistance() {
+
+    var hammingDistance = new HammingDistance();
+    Assertions.assertEquals(2, hammingDistance.hammingDistance(1, 4));
+
   }
 }
