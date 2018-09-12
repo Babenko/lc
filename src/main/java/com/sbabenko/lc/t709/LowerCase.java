6ee3bcda05574eabe3773be95f9c223e54fd89e4
Sem Babenko <babenkosemen@gmail.com>
2018-09-12 17:47:10 +0300

    Solution for task num 709.

--- src/main/java/com/sbabenko/lc/t709/LowerCase.java
+++ src/main/java/com/sbabenko/lc/t709/LowerCase.java
@@ -1,12 +1,26 @@
 package com.sbabenko.lc.t709;
 
+import java.io.ByteArrayOutputStream;
+
 /**
  * Created by Sem Babenko.
  */
 public class LowerCase {
 
+  private static final int OFFSET = 32;
+  private static final int LOW_BORDER = 64;
+  private static final int UPPER_BORDER = 91;
+
   public String toLowerCase(String str) {
-    return null;
+
+    return new String(str.codePoints().map(this::mapToLower)
+            .collect(ByteArrayOutputStream::new,
+                    (baos, val) -> baos.write((byte)val),
+                    (b1, b2) -> b1.write(b2.toByteArray(), 0, b2.size())).toByteArray());
+  }
+
+  private int mapToLower(int val) {
+    return val > LOW_BORDER && val < UPPER_BORDER ? val + OFFSET : val;
   }
 
 }
